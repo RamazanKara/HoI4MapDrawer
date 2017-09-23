0f46ee354bd1e8e7434aa2612e96fae860f44c7f
Idhrendur <idhrendur@hotmail.com>
2017-09-23 16:20:21 -0700

    Make map occupy whole window

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,36 +29,13 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.VerticalSplit = new System.Windows.Forms.SplitContainer();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.StatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.pbSource = new System.Windows.Forms.PictureBox();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.VerticalSplit)).BeginInit();
-            this.VerticalSplit.Panel1.SuspendLayout();
-            this.VerticalSplit.Panel2.SuspendLayout();
-            this.VerticalSplit.SuspendLayout();
             this.statusStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbSource)).BeginInit();
             this.SuspendLayout();
-            // 
-            // VerticalSplit
-            // 
-            this.VerticalSplit.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.VerticalSplit.Location = new System.Drawing.Point(0, 0);
-            this.VerticalSplit.Name = "VerticalSplit";
-            // 
-            // VerticalSplit.Panel1
-            // 
-            this.VerticalSplit.Panel1.Controls.Add(this.statusStrip1);
-            // 
-            // VerticalSplit.Panel2
-            // 
-            this.VerticalSplit.Panel2.AutoScroll = true;
-            this.VerticalSplit.Panel2.Controls.Add(this.pbSource);
-            this.VerticalSplit.Size = new System.Drawing.Size(705, 467);
-            this.VerticalSplit.SplitterDistance = 235;
-            this.VerticalSplit.TabIndex = 0;
             // 
             // statusStrip1
             // 
@@ -101,16 +78,13 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(705, 467);
-            this.Controls.Add(this.VerticalSplit);
+            this.AutoScroll = true;
+            this.Controls.Add(this.pbSource);
+            this.Controls.Add(this.statusStrip1);
             this.KeyPreview = true;
             this.Name = "Form1";
             this.Text = "Province Mapper";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.VerticalSplit.Panel1.ResumeLayout(false);
-            this.VerticalSplit.Panel1.PerformLayout();
-            this.VerticalSplit.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.VerticalSplit)).EndInit();
-            this.VerticalSplit.ResumeLayout(false);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbSource)).EndInit();
@@ -120,7 +94,6 @@
 
         #endregion
 
-        private System.Windows.Forms.SplitContainer VerticalSplit;
         private System.Windows.Forms.PictureBox pbSource;
         private System.Windows.Forms.ToolTip toolTip1;
         private System.Windows.Forms.StatusStrip statusStrip1;
