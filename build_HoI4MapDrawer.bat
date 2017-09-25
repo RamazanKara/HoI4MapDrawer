24b4b625496d51a79031f0840895605cd28dd788
Idhrendur <idhrendur@hotmail.com>
2017-09-24 19:14:09 -0700

    Fix build script

--- build_HoI4MapDrawer.bat
+++ build_HoI4MapDrawer.bat
@@ -4,8 +4,8 @@ call "%VS140COMNTOOLS%\vsvars32.bat"
 msbuild.exe "HoI4MapDrawer.sln" /p:Configuration=Release /m
 mkdir "HoI4MapDrawer-%version%\"
 copy "bin\Release\ProvinceMapper.exe" "HoI4MapDrawer-%version%\HoI4MapDrawer.exe"
-copy "FAQ.txt" "ProvinceMapper-%version%\FAQ.txt"
-copy "readme.txt" "ProvinceMapper-%version%\readme.txt"
+copy "FAQ.txt" "HoI4MapDrawer-%version%\FAQ.txt"
+copy "readme.txt" "HoI4MapDrawer-%version%\readme.txt"
 
 cd "HoI4MapDrawer-%version%"
 call "%SEVENZIP_LOC%\7z.exe" a -tzip -r "..\HoI4MapDrawer-%version%.zip" "*" -mx5
